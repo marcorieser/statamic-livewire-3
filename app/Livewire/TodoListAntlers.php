e28f4663b27e59fbda20be387d31e37ba0769d9d
Marco Rieser <marco@rieser.dev>
2023-09-07 14:16:00 +0200

    wip

---
+++ app/Livewire/TodoListAntlers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TodoListAntlers extends Component
+{
+    public $title = 'Antlers';
+    public $todos = [
+        'Todo 1',
+        'Todo 2',
+        'Todo 3',
+    ];
+    public $todo = 'New Todo';
+
+    public function mount($passed = null)
+    {
+        dump([self::class, $passed, gettype($passed)]);
+    }
+
+    public function render()
+    {
+        return view('livewire.todo-list-antlers');
+    }
+
+    public function add()
+    {
+        $this->todos[] = $this->todo;
+        $this->todo = 'After submit by Livewire';
+    }
+}
---
+++ resources/views/livewire/todo-list-blade.blade.php
@@ -0,0 +1,40 @@
+<div x-data="{todos: @entangle('todos').live, todo: @entangle('todo')}">
+    <h2>{{$title}}</h2>
+    <ul>
+        @foreach($todos as $value)
+            <li>{{ $value }}</li>
+        @endforeach
+    </ul>
+    <div class="flex gap-4">
+        <form @submit.prevent="todos.push(todo); todo = 'After submit by Alpine'">
+            <input type="text" x-model="todo"> <br>
+            <button type="submit" class="bg-gray-700 p-2 text-white">Add Todo with Alpine</button>
+        </form>
+
+        <form wire:submit.prevent="add">
+            <input type="text" wire:model="todo"> <br>
+            <button type="submit" class="bg-gray-700 p-2 text-white">Add Todo with Livewire</button>
+        </form>
+    </div>
+    <hr>
+    <div>
+        <h4>Rendered by <span>Alpine</span> (no binding)</h4>
+        <div class="flex gap-4 items-center">
+            <span x-text="todo"></span>
+            <input type="text" :value="todo">
+        </div>
+    </div>
+    <div>
+        <h4>Rendered by <span>Livewire</span> (no binding)</h4>
+        <div class="flex gap-4 items-center">
+            <span>{{$todo}}</span>
+            <input type="text" value="{{$todo}}">
+        </div>
+    </div>
+
+    <script>
+        document.addEventListener('livewire:initialized', function () {
+            @this.$set('title', @this.title + ' (modified by JS)');
+        })
+    </script>
+</div>
